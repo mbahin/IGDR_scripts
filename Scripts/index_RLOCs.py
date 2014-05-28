be0450b5445f056d5b2f5f8cd596dc9b9684bde5
mbahin <mbahin@univ-rennes1.fr>
2014-05-28 09:15:55 +0200

    Creating an index for the ENSCAFgs

--- Scripts/index_RLOCs.py
+++ Scripts/index_RLOCs.py
@@ -9,40 +9,39 @@
 import sys, re
 
 input = open(sys.argv[1],'r')
-output = open(sys.argv[2],'w')
+RLOC_output = open(sys.argv[2],'w')
+ENSCAFG_output = open(sys.argv[3], 'w')
 
 RLOCs = {}
+ENSCAFGs = {}
 for line in input:
     rloc = line.split('\t')[8].split('"')[1]
-    gene_name = line.split('\t')[8].split('"')[3]
+    name = line.split('\t')[8].split('"')[3]
     # Removing the transcript number if present
-    match = re.match(r'(.*)_[0-9]*$',gene_name)
+    match = re.match(r'(.*)_[0-9]*$',name)
     if match:
-        gene_name = match.group(1)
+        name = match.group(1)
     enscafg = line.split('\t')[17].split('"')[1]
     if not RLOCs.has_key(rloc):
-        RLOCs[rloc] = {}
-        RLOCs[rloc]['enscafgs'] = []
-        RLOCs[rloc]['gene_names'] = []
-    if enscafg not in RLOCs[rloc]['enscafgs']:
-        RLOCs[rloc]['enscafgs'].append(enscafg)
-        if RLOCs[rloc]['gene_names'] == []:
-            if (gene_name.startswith('ENSCAFG')) or (gene_name.startswith('CFRNASEQ')):
-                RLOCs[rloc]['gene_names'].append('No_name')
-            else:
-                RLOCs[rloc]['gene_names'].append(gene_name)
-        #elif (RLOCs[rloc]['gene_names'][0] != gene_name) and (RLOCs[rloc]['gene_names'][0] != 'No_name') and (not gene_name.startswith('CFRNASEQ')):
-            #print enscafg, gene_name, RLOCs[rloc]['gene_names']
-        elif gene_name not in RLOCs[rloc]['gene_names'] and not ((gene_name.startswith('ENSCAFG')) or (gene_name.startswith('CFRNASEQ'))):
-            if RLOCs[rloc]['gene_names'][0] == 'No_name':
-                RLOCs[rloc]['gene_names'] == [gene_name]
-            else:
-                RLOCs[rloc]['gene_names'].append(gene_name)
+        RLOCs[rloc] = []
+    if enscafg not in RLOCs[rloc]:
+        RLOCs[rloc].append(enscafg)
+    if not name.startswith('ENSCAFG') and not name.startswith('CFRNASEQ'):
+        if ENSCAFGs.has_key(enscafg) and ENSCAFGs[enscafg] != name:
+            sys.exit('Two names for ENSCAFG '+enscafg+'! Aborting.')
+        ENSCAFGs[enscafg] = name
+    else:
+        if ENSCAFGs.has_key(enscafg) and ENSCAFGs[enscafg] != 'No_name':
+            sys.exit('Two names for ENSCAFG '+enscafg+'! Aborting.')
+        ENSCAFGs[enscafg] = 'No_name'
 
-for i in sorted(RLOCs):
-    output.write(i)
-    #for j in range(0, len(RLOCs[i]['enscafgs'])):
-    output.write('\t'+str(RLOCs[i]['enscafgs'])+'\t'+str(RLOCs[i]['gene_names'])+'\n')
+# Writing index files
+for rloc in sorted(RLOCs):
+    RLOC_output.write(rloc+'\t'+','.join(RLOCs[rloc])+'\n')
+
+for i in sorted(ENSCAFGs):
+    ENSCAFG_output.write(i+'\t'+ENSCAFGs[i]+'\n')
 
 input.close()
-output.close()
+RLOC_output.close()
+ENSCAFG_output.close()
