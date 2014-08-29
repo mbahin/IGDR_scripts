7b377c241b5caab837edc1e6fd1c3d38fd5b0a26
mbahin <mbahin@univ-rennes1.fr>
2014-08-29 09:46:06 +0200

    Modifying fasta filename

--- Fusion_genes/CRAC/process_chimCT_output.py
+++ Fusion_genes/CRAC/process_chimCT_output.py
@@ -274,5 +274,5 @@ monoFeat_file.close()
 twoFeat_file.close()
 
 # Creating symbolic links to the spanning split reads fasta file and to the CRAC output directory of the analyse
-os.symlink(options.input+'/'+options.sample_name+'.fa','link_to_spanning_split_reads.ln.fasta')
+os.symlink(options.input+'/'+options.sample_name+'.fa',options.sample_name+'.fa')
 os.symlink(options.crac_dir,'link_to_BAM_files_directory.ln')
