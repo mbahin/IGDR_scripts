45733045a2ffac80242372347233a54201dfbca6
mbahin <mbahin@univ-rennes1.fr>
2014-09-15 09:35:04 +0200

    Adding a gen_name dictionary to get_indexes.py module

--- Scripts/get_indexes.py
+++ Scripts/get_indexes.py
@@ -6,6 +6,7 @@
 
 RLOCs = {}
 ENSCAFGs = {}
+gene_names = {}
 
 def index_RLOCs():
     #####
@@ -19,6 +20,7 @@ def index_RLOCs():
     #   - 'BROAD_name' : the BROAD gene name.
     #   - 'consensus_names' (list) : the (uniqued) gene name(s) list from Ensembl and the BROAD.
     #   - 'RLOC' : the RLOC corresponding to the ENSCAFG.
+    # The gene names are indexed into the dictionary 'gene_names' to link to the ENSCAFGs.
     #####
 
     with open('/home/genouest/umr6061/recomgen/dog/data/canFam3/annotation/Correspondence_Indexes/RLOCs_index.txt','r') as RLOCs_file:
@@ -33,17 +35,22 @@ def index_RLOCs():
             RLOCs[rloc]['orthologous'] = line.split('\t')[2]
             RLOCs[rloc]['biotypes'] = line.rstrip().split('\t')[3].split(',')
 
-with open('/home/genouest/umr6061/recomgen/dog/data/canFam3/annotation/Correspondence_Indexes/ENSCAFGs_index.txt','r') as ENSCAFGs_file:
-    # Ignoring the header
-    ENSCAFGs_file.readline()
-    # Processing each line
-    for line in ENSCAFGs_file:
-        enscafg = line.split('\t')[0]
-        if not ENSCAFGs.has_key(enscafg):
-            ENSCAFGs[enscafg] = {}
-        ENSCAFGs[enscafg]['Ensembl_name'] = line.split('\t')[1]
-        ENSCAFGs[enscafg]['BROAD_name'] = line.split('\t')[2]
-        ENSCAFGs[enscafg]['consensus_names'] = line.split('\t')[3].split('|')
-        ENSCAFGs[enscafg]['RLOC'] = line.rstrip().split('\t')[4]
+    with open('/home/genouest/umr6061/recomgen/dog/data/canFam3/annotation/Correspondence_Indexes/ENSCAFGs_index.txt','r') as ENSCAFGs_file:
+        # Ignoring the header
+        ENSCAFGs_file.readline()
+        # Processing each line
+        for line in ENSCAFGs_file:
+            enscafg = line.split('\t')[0]
+            if not ENSCAFGs.has_key(enscafg):
+                ENSCAFGs[enscafg] = {}
+            ENSCAFGs[enscafg]['Ensembl_name'] = line.split('\t')[1]
+            ENSCAFGs[enscafg]['BROAD_name'] = line.split('\t')[2]
+            ENSCAFGs[enscafg]['consensus_names'] = line.split('\t')[3].split('|')
+            if ENSCAFGs[enscafg]['consensus_names'] != ['NA']:
+                for name in line.split('\t')[3].split('|'):
+                    if not gene_names.has_key(name):
+                        gene_names[name] = []
+                    gene_names[name].append(enscafg)
+            ENSCAFGs[enscafg]['RLOC'] = line.rstrip().split('\t')[4]
 
 index_RLOCs()
